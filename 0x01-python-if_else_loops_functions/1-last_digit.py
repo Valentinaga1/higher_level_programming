8284842a215fd5347d69bd9e61c600a33b9955a0
Valentinaga1 <1881@holbertonschool.com>
2020-09-01 18:15:54 +0000

    Check last digit is >5, <6 o is zero

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number > 0:
+    lastDigit = number % 10
+else:
+    lastDigit = number % -10
+
+if lastDigit > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, lastDigit))
+elif lastDigit == 0:
+    print("Last digit of {} is {} and is 0".format(number, lastDigit))
+elif lastDigit < 6:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastDigit))
