b892dc33a65eab98bf970649f88ef3ffd97c82c4
Valentinaga1 <1881@holbertonschool.com>
2020-09-03 21:10:47 +0000

    Prints all the names defined by the compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,7 @@
 if __name__ == "__main__":  # el codi no se debe exe cuando se importa
     import hidden_4
     x = dir(hidden_4)
-    if x[0] != "__":
-        print(x)
+    for i in x:
+        if x[:1] != "__":
+            print("\n".join(x))
 
