2a26cb37f2b2bbab1bebb50728ddd64e263d754e
Valentinaga1 <1881@holbertonschool.com>
2020-09-03 19:22:33 +0000

    Program that imports all the functions from a file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,13 +2,15 @@
 if __name__ == "__main__":  # el codi no se debe exe cuando se importa
     from calculator_1 import add, sub, mul, div
     import sys
-    a = sys.argv[1]
-    b = sys.argv[3]
-    operator = sys.argv[2]
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+        
+    a = sys.argv[1]
+    b = sys.argv[3]
+    operator = sys.argv[2]
+
     if operator == "+":
         print("{} {} {} = {}".format(a, operator, b, add(a, b)))
     elif operator == "-":
@@ -19,3 +21,4 @@ if __name__ == "__main__":  # el codi no se debe exe cuando se importa
         print("{} {} {} = {}".format(a, operator, b, div(a, b)))
     else:
         print("Unknown operatorerator. Available operators: +, -, * and /")
+        exit(1)
