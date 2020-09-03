067bac8f518df69bcef5df598c1999bc39987822
Valentinaga1 <1881@holbertonschool.com>
2020-09-03 19:34:41 +0000

    Program that imports all the functions from a file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,17 +6,18 @@ if __name__ == "__main__":  # el codi no se debe exe cuando se importa
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = sys.argv[1]
-    b = sys.argv[3]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     operator = sys.argv[2]
+
     if operator == "+":
-        print("{} {} {} = {}".format(int(a), operator, int(b), add(int(a), int(b))))
+        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
     elif operator == "-":
-        print("{} {} {} = {}".format(int(a), operator, int(b), sub(int(a), int(b))))
+        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
     elif operator == "*":
-        print("{} {} {} = {}".format(int(a), operator, int(b), mul(int(a), int(b))))
+        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
     elif operator == "/":
-        print("{} {} {} = {}".format(int(a), operator, int(b), div(int(a), int(b))))
+        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
     else:
         print("Unknown operatorerator. Available operators: +, -, * and /")
         exit(1)
