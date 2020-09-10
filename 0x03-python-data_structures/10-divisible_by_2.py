5ce76e75edee3c6c6d05e069d3f7c3ebd5dee6f3
Valentinaga1 <1881@holbertonschool.com>
2020-09-10 01:15:02 +0000

    Function that finds all multiples of 2 in a list and return a new list.

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    for number in my_list:
-        if number % 2 == 0:
-            my_list = True
-        else:
-            my_list = False
-    return my_list
+def divisible_by_2(my_list=[]):  # %retorna true
+    new_list = []
+    for n in my_list:
+        new_list.append(n % 2 == 0)
+    return new_list
