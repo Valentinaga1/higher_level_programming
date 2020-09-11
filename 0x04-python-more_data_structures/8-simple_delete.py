a1fadf45eaa67940ddd2e4e1fafd442c51601b07
Valentinaga1 <1881@holbertonschool.com>
2020-09-11 16:14:23 +0000

    Function that deletes a key in a dictionary. more ways

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -8,3 +8,5 @@ def simple_delete(a_dictionary, key=""):
 #   if a_dictionary.get(key) is not None:
 #       del a_dictionary[key]
 #   return a_dictionary
+# get mira si una key existe o no, si no return None
+# get(key,lo que quiero que me retorne si no quiero que retorne None)
