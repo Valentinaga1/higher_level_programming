aa4a79acc0b6f3fc9aae6b20b9f70fae280a747b
Valentinaga1 <1881@holbertonschool.com>
2020-09-11 16:23:18 +0000

    Function that returns a new dictionary with all values multiplied by 2. more ways

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -7,5 +7,11 @@ def multiply_by_2(a_dictionary):
 
 # Another way: dict compreh
 # return {key: a_dictionary[key] * 2 for key in a_dictionary}
+# así a_dictionary[key] accedo al dic en cada key para traer el value
+# si no accedo al valor obtengo solo la key
+
 # Another way
 # return {key: 2 * v for key, v in a_dictionary.items()}
+# .item devuelve una lista de tuplas clave valor
+# estoy iterando sobre la key y el value
+# le estoy poniendo la misma clave * 2
