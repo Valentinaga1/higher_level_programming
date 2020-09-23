66c0c8b6dd4059c797e6f0d78ce7bc046a0a4563
Valentinaga1 <1881@holbertonschool.com>
2020-09-23 14:32:58 +0000

    class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,27 +19,50 @@ class Square():
     # Property. Instance atribute private
     @property
     def size(self):
+        '''
+            property  to retrieve it
+        '''
         return self.__size
 
     @size.setter
     # setter
     def size(self, value):
-        def size(self, value):
-            if type(value) != int:
-                    raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+        '''
+            property  setter to set it
+            Args:
+            value(int)
+        '''
+        return self.__size
+
+    def size(self, value):
+        '''
+            property  setter to set it
+            Args:
+            value(int)
+        '''
+        if type(value) != int:
+                raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
 
     # Property. atribute public insctance
     @property
     def position(self):
+        '''
+        property  to retrieve it
+        '''
         return self.__position
 
     # setter
     @position.setter
     def position(self, value):
+        '''
+            property  setter to set it
+            Args:
+            value(int)
+        '''
         if type(value) != tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif len(tuple) != 2:
@@ -48,9 +71,16 @@ class Square():
             self.__position = value
 
     def area(self):
+        '''
+            Method that returns the current square area
+        '''
         return self.__size**2
 
     def my_print(self):
+        '''
+            Method that prints in stdout the square with
+            the character #
+        '''
         if self.__size is 0:
             print()
         else:
