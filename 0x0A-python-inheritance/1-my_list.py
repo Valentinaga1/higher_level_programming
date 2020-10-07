a60b046367b4a63e92c2010e615ab40571d82c22
Valentinaga1 <1881@holbertonschool.com>
2020-10-07 01:58:58 +0000

    Fixed

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,9 +5,6 @@
 
 class MyList(list):
     """Creatin MyList class that inherits form list
-
-    Args:
-        list (list): list to print
     """
     def print_sorted(self):
         print(sorted(self))
