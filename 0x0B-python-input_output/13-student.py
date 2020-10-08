47e60269658979508a1637a5fa35b7903cdd13c7
Valentinaga1 <1881@holbertonschool.com>
2020-10-08 18:48:51 +0000

    13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Module that defines a clas Student
+"""
+
+
+class Student():
+    """class Student that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dictionary = {}
+        if type(attrs) == list:
+            for i in attrs:
+                if type(i) == str and i in self.__dict__.keys():
+                    dictionary[i] = self.__dict__[i]
+            return dictionary
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            self.__dict__[key] = value
