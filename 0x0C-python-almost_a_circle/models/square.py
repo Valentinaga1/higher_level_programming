f27c87e4ca63cce39eb7eb7bbb9873a97ceb2900
Valentinaga1 <1881@holbertonschool.com>
2020-10-13 20:54:28 +0000

    Fixing atributos

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -13,6 +13,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Create a Square instance
         """
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
