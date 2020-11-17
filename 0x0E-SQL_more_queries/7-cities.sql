f1ed5a158e2bb1d958d841566aacc6067089c0e5
Valentinaga1 <1881@holbertonschool.com>
2020-11-17 19:21:18 +0000

    tasks 0-8

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,9 +5,9 @@
 -- name VARCHAR(256) cant be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-    id INT PRIMARY KEY  AUTO_INCREMENT NOT NULL,
+    id INT PRIMARY KEY  AUTO_INCREMENT,
     state_id INT NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES states(id),
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
     name VARCHAR(256) NOT NULL
 );
 
