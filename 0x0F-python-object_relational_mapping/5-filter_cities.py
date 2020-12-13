a6a355279840f0c4d80c793aeddde241a2af5fef
Valentinaga1 <1881@holbertonschool.com>
2020-12-13 20:50:11 +0000

    Listing all the cities of a states

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that takes in the name of a state as an argument and lists
+    all cities of that state, using the database hbtn_0e_4_usa
+    """
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    serv = MySQLdb.connect(host="localhost",  port=3306,
+                           user=sys.argv[1], password=sys.argv[2],
+                           database=sys.argv[3])
+
+    c = serv.cursor()
+    stateName = sys.argv[4]
+    c.execute("SELECT  cities.name FROM cities\
+               JOIN states ON cities.state_id = states.id\
+               WHERE states.name=%s ORDER BY cities.id ASC", (stateName,))
+    rows = c.fetchall()
+    for row in rows:
+        print(','.join(row), end=" ", sep=" ")
+    print()
+    c.close()
+    serv.close()
