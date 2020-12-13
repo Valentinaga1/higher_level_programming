7528409e1001ca300f7c37a3d45a91d419cffe8b
Valentinaga1 <1881@holbertonschool.com>
2020-12-13 21:05:57 +0000

    Adding a state to the table with sqlachemy

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
 if __name__ == "__main__":
 
     import sys
@@ -9,9 +10,9 @@ if __name__ == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(engine)
     state = State(name="Louisiana")
     session.add(state)
     session.commit()
