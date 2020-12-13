c8f94de0a76f9d919f983b50bba0cd33fd57f88a
Valentinaga1 <1881@holbertonschool.com>
2020-12-13 02:10:56 +0000

    Displays all names that match with the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Script  that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    serv = MySQLdb.connect(host="localhost",  port=3306,
+                           user=sys.argv[1], password=sys.argv[2],
+                           database=sys.argv[3])
+
+    c = serv.cursor()
+    stateName = sys.argv[4]
+    c.execute(
+        "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(
+            stateName))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    serv.close()
+
+# Another way
+# cursor.execute("SELECT * FROM states ORDER BY id;")
+#    query_rows = cursor.fetchall()
+#    for row in query_rows:
+#        if row[1] == sys.argv[4]:
+#            print("{}".format(row))
