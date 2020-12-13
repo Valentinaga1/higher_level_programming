cfcb03c8eca8ba1ea1153eae9dd9549f02726384
Valentinaga1 <1881@holbertonschool.com>
2020-12-13 01:54:23 +0000

    Listing all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+""" Script that lists all states from the database hbtn_0e_0_usa
+"""
 if __name__ == "__main__":
     import sys
     import MySQLdb
-    """ Script that lists all states from the database hbtn_0e_0_usa
-    """
 
     serv = MySQLdb.connect(host="localhost",  port=3306,
                            user=sys.argv[1], password=sys.argv[2],
