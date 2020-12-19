aeeb9025ab14a0096390a42305ae1f0c2022ea5f
Valentinaga1 <1881@holbertonschool.com>
2020-12-19 03:59:34 +0000

    sending a header variable

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+# Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+#!/bin/bash
+curl -s -X POST -d "X-HolbertonSchool-User-Id=98" $1
