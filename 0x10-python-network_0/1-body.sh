53f29c289852abf5514d52bfbbe90ac6d73362a3
Valentinaga1 <1881@holbertonschool.com>
2020-12-19 03:06:03 +0000

    body size of the repsonse

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a GET request to the URL, displaying the body size
+#Bash script that takes in a URL, sends a GET request to the URL, displaying the body size
 curl -s -L $1 -X GET
