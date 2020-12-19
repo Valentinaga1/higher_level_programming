16f9805639d7d54d7923a3e2e6b7ab0c8cfef9c9
Valentinaga1 <1881@holbertonschool.com>
2020-12-19 03:29:20 +0000

    Accepted methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
 #!/bin/bash
-curl -i -X OPTIONS $1 | grep Allow | cut -d" " -f2
+curl -i - -X OPTIONS $1 | grep Allow | cut -d" " -f2
