8356c64a461b0590cec7a5ede1f31c293765d607
Valentinaga1 <1881@holbertonschool.com>
2020-12-21 01:41:31 +0000

    displaying then body of the response

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Bash script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s $1 -d @"$2" -H "Content-Type: application/json"
+curl -s -X POST --data @"2" -H "Content-Type: application/json; charset=UTF-8" $1
