fc2f7a74b4490efa3dfa2e5c5aabc33b83089338
Valentinaga1 <1881@holbertonschool.com>
2021-01-11 15:26:01 -0500

    fixing url

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,7 +6,11 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    user = sys.argv[1]
+    pssword = sys.argv[2]
     url = "https://api.github.com/user"
-    req = requests.get(url, auth=(sys.argv[1], sys.argv[2])).json()
 
-    print(req.get("id"))
+    req = requests.get(url, auth=(user, pssword))
+    req_json_rep = req.json()
+
+    print(req_json_rep.get("id", "None"))
