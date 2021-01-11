cacaf4324f568d40cc1b79763589571f47f009ff
Valentinaga1 <1881@holbertonschool.com>
2021-01-11 14:53:17 -0500

    second commit

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that takes your Github credentials (username and password) and uses
+the Github API to display your id"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+	url = "https://api.github.com/user"
+	req = requests.get(url, auth=(sys.argv[1], sys.argv[2])).json()
+
+	print(req.get("id"))
