44b23875dc452428ae136648ce2c000090c48198
Valentinaga1 <1881@holbertonschool.com>
2021-02-02 16:05:13 -0500

    Fixing function position and NaN string

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
+if (parseInt(process.argv[2]) && parseInt(process.argv[3])) {
+  console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
+}
+else {
+  console.log('NaN');
+}
 function add(a, b) {
   return a + b;
 }
-const a = parseInt(process.argv[2]);
-const b = parseInt(process.argv[3]);
-console.log(add(a, b));
