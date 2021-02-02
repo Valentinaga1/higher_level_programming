79c7572cadf3b81424ed8764d3b49f1c9ce70237
Valentinaga1 <1881@holbertonschool.com>
2021-02-02 14:35:18 -0500

    Fixing name of the variable

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let arrayStrings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for (let i = 0; i < array_of_strings.length; i++) {
-  console.log(array_of_strings[i]);
+for (let i = 0; i < arrayStrings.length; i++) {
+  console.log(arrayStrings[i]);
 }
