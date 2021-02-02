067d1b9d20d9597feb5eb44c6bd8e3f0200c397a
Valentinaga1 <1881@holbertonschool.com>
2021-02-02 14:20:31 -0500

    changing varaible

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-let three_lines = 'C is fun\nPython is cool\nJavascript is amazing';
+const three_lines = 'C is fun\nPython is cool\nJavascript is amazing';
 console.log(three_lines);
