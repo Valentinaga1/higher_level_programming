6a436736b95e52717f0509bb17c4e5c4575b2615
Valentinaga1 <1881@holbertonschool.com>
2021-02-02 14:50:32 -0500

    Fixing variable declaration

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if ((process.argv.length === 2) || (process.argv.length === 3)) {
   console.log(0);
 } else {
-  let arr = Array()
+  const arr = Array()
   for (let i = 2; i < process.argv.length; i++) {
 
     arr.push(process.argv[i]);
