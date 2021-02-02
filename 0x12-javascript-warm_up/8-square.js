c47c82081c7430886c9f40dea19500f78d4893b4
Valentinaga1 <1881@holbertonschool.com>
2021-02-02 14:47:36 -0500

    fixing variable declaration

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const x = '';
+let x = '';
 if (parseInt(process.argv[2])) {
   for (let i = 0; i < process.argv[2]; i++) {
-    x = "";
+    x = '';
     for (let j = 0; j < process.argv[2]; j++) {
       x += 'X';
     }
