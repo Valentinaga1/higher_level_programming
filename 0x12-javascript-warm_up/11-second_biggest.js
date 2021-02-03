7c93717d65811b9bdbf6090fa3cf0dbcada22b84
Valentinaga1 <1881@holbertonschool.com>
2021-02-02 19:10:10 -0500

    Fixing array sintaxis

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,8 @@
 if ((process.argv.length === 2) || (process.argv.length === 3)) {
   console.log(0);
 } else {
-  const arr = Array()
+  const arr = [];
   for (let i = 2; i < process.argv.length; i++) {
-
     arr.push(process.argv[i]);
     arr.sort();
   }
