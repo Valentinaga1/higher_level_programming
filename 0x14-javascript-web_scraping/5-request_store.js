f37847391ffeb499a6b587e3c72a561b3a44d39f
Valentinaga1 <1881@holbertonschool.com>
2021-02-09 14:21:15 -0500

    Fixing requirement

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const request = require('request');
 const fs = require('fs');
+const request = require('request');
 const url = process.argv[2];
 const nameFile = process.argv[3];
-let text;
 
 request(url, errorFunc);
 
@@ -11,13 +10,10 @@ function errorFunc (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    text = body;
-    fs.writeFile(nameFile, text, 'utf-8', errorFunc2);
-
-    function errorFunc2 (err) {
+    fs.writeFile(nameFile, body, 'utf-8', (err) => {
       if (err) {
         console.log(err);
       }
-    }
+    });
   }
 }
